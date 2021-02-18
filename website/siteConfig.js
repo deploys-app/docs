47d7292fe59e4bed819098a7ee1afe1d9535d312
Chukkrit Visitsaktavorn <chukkrit.juk@gmail.com>
2021-02-18 16:42:37 +0700

    update project name

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'Deploys.app',
   tagline: 'Designed for developers, Built for business.',
-  url: 'https://doc.deploys.app',
-  baseUrl: '/',
+  url: 'https://deploys-app.github.io',
+  baseUrl: '/docs/',
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'deploys-doc',
-  organizationName: 'moonrhythm',
+  projectName: 'docs',
+  organizationName: 'deploys-app',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
