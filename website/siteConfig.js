8c46a77e3f512ec83ebf4ccfa0a54dd938126fc4
Chukkrit Visitsaktavorn <chukkrit.juk@gmail.com>
2021-02-18 16:39:17 +0700

    fixed project name

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,7 +30,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'deploys.app-doc',
+  projectName: 'deploys-doc',
   organizationName: 'moonrhythm',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
